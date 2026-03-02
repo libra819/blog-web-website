9b23bfac7554a3cc9722aba51718f26cf7dc62a1
libra <libra@hying.com.tw>
2026-03-02 15:34:15 +0800

    feat: 更新文章詳細頁面，將 post() 轉為 JSON 格式並優化狀態管理

--- src/app/pages/post-detail/post-detail.ts
+++ src/app/pages/post-detail/post-detail.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink, Router } from '@angular/router';
 import { DatePipe } from '@angular/common';
+import { Post, PostService } from '../../services/post';
 
 @Component({
   selector: 'app-post-detail',
@@ -12,20 +13,36 @@ import { DatePipe } from '@angular/common';
 export class PostDetail implements OnInit {
   private route = inject(ActivatedRoute);
   private router = inject(Router);
-  
+  private postService = inject(PostService);
+
   // 存放文章資料的變數
-  post: any = null;
+  post = signal<Post>({
+    id: 0,
+    uuid: '',
+    category: '',
+    author_id: '',
+    author_name: '', // 假設後端會 JOIN members 表並回傳作者名稱
+    title: '',
+    summary: '',
+    content: '',
+    tags: '', // 資料庫的 varchar，例如 "Angular,Express,JWT"
+    created_at: '', // 後端回傳的 ISO 8601 時間字串
+    updated_at: '',
+  });
 
   ngOnInit(): void {
-    // 取得路由參數中的 id
-    this.route.paramMap.subscribe(params => {
-      const postId = params.get('id');
-      this.loadPost(postId);
+    // 取得路由的參數，這裡假設路由定義為 /post/:id
+    this.route.paramMap.subscribe((params) => {
+      const id = params.get('id');
+      if (id) {
+        this.loadPost(id);
+      }
     });
   }
 
   // 模擬從 API 讀取文章
-  loadPost(id: string | null) {
+  loadPost(id: string | number) {
+    /*
     // 這裡放一筆假資料供切版使用，之後會換成呼叫 post.service.ts
     this.post = {
       id: id,
@@ -50,13 +67,23 @@ export class PostDetail implements OnInit {
         <p>接下來我們還會討論如何實作路由守衛 (Route Guard) 來保護特定的頁面...</p>
       `,
       tags: 'Angular,Express,JWT'.split(',') // 將字串轉成陣列
-    };
+    };*/
+    this.postService.getPostById(id).subscribe({
+      next: (data) => {
+        this.post.set(data);
+      },
+      error: (err) => {
+        console.error('取得文章失敗', err);
+        // 如果文章不存在或發生錯誤，可以導回首頁或顯示錯誤訊息
+        this.router.navigate(['/']);
+      },
+    });
   }
 
   searchByTag(tag: string) {
     // 搜尋tag，回到首頁並顯示搜尋tag的結果
-    console.log('搜尋 tag:', tag);
-      // 這裡可以使用 Angular 的 Router 來導航回首頁，並帶上搜尋參數
+    console.log('搜尋:', tag);
+    // 這裡可以使用 Angular 的 Router 來導航回首頁，並帶上搜尋參數
     // 例如：this.router.navigate(['/'], { queryParams: { q: tag } });
     this.router.navigate(['/'], { queryParams: { tags: tag } });
   }
